88e9110ce18650c7e3f8f91d1e334712419dca5b
Eivind Hystad <eivhys@hotmail.com>
2018-10-02 16:04:39 +0200

    fixed package name and removed dev stuff

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 //index.js
 const winfo = require('./build/Release/winfo.node');
-let test = true;
-if (test) {
-    console.log('Object:', winfo);
-    console.log('Title:', winfo.title());
-    console.log('id:' + winfo.id()); // note yet implemented
-    console.log('x:' + winfo.x());
-    console.log('y:' + winfo.y());
-    console.log('width:' + winfo.width());
-    console.log('height' + winfo.height());
-}
 
 module.exports = winfo;
